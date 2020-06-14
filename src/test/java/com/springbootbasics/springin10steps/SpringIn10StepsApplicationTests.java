03b85edf176a86219860e75af5aa362038fad909
Manas Mukherjee <mmanas19@gmail.com>
2020-06-13 18:46:17 -0700

    first checking

---
+++ src/test/java/com/springbootbasics/springin10steps/SpringIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootbasics.springin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
